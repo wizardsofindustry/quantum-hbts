bf5112ef697b1021972e9a6a6ac57f34948432c0
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-12 18:55:37 +0200

    qsa: Compile top-level HTTP endpoint schema package

--- hbts/app/wsgi/schema/__init__.py
+++ hbts/app/wsgi/schema/__init__.py
@@ -1,4 +1,7 @@
 from .error import Error
 from .health import Health
+from .securetimestamp import SecureTimestamp
 from .servicenotavailable import ServiceNotAvailable
+from .timestampexists import TimestampExists
+from .timestampingrequest import TimestampingRequest
 from .version import Version
