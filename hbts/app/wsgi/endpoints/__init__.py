7cbfbe71a0a55542f3815c5d583269f85e611736
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-12 18:55:37 +0200

    qsa: Compile endpoints from OpenAPI definition

--- hbts/app/wsgi/endpoints/__init__.py
+++ hbts/app/wsgi/endpoints/__init__.py
@@ -2,11 +2,13 @@ from werkzeug.routing import Map
 
 from .health import HealthEndpoint
 from .version import VersionEndpoint
+from .timestamp import TimestampEndpoint
 
 
 urlpatterns = Map([
     HealthEndpoint.as_rule(),
     VersionEndpoint.as_rule(),
+    TimestampEndpoint.as_rule(),
 ])
 
 
