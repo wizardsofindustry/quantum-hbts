2af2e5a0e34b273df7059030d1abe040614c43fc
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-12 19:48:04 +0200

    qsa: Compile top-level controllers package.

---
+++ hbts/app/ctrl/__init__.py
@@ -0,0 +1,4 @@
+"""Contains all controller implementations that are used by the WSGI
+appication. See also :class:`~hbts.app.wsgi.application.WSGIApplication`.
+"""
+from .timestamp import TimestampCtrl
