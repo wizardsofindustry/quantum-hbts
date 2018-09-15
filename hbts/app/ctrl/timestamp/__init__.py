e0d3e546b928c13ed1332d0e3f9d8800f5e2f5d4
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-15 15:04:53 +0200

    qsa: Initial TimestampCtrl specification

--- hbts/app/ctrl/timestamp/__init__.py
+++
@@ -1,5 +0,0 @@
-"""The :mod:`~hbts.app.ctrl.timestamp` module holds the base
-class and concrete implementation of :class:`TimestampCtrl`.
-"""
-from .base import BaseTimestampCtrl
-from .impl import TimestampCtrl
